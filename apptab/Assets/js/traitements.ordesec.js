ba6f5f88f70ac26e273ebc96f41d7c4c4d447568
El-Cid-Campeador <mandaniainaandriamanantena@gmail.com>
2024-02-23 12:12:34 +0300

    Update

--- apptab/Assets/js/traitements.ordesec.js
+++ apptab/Assets/js/traitements.ordesec.js
@@ -7,43 +7,53 @@ function checkdel(id) {
     $('.Checkall').prop("checked", false);
 }
 
-function GetListLOADOTHER() {
+function GetListLOAD() {
     let formData = new FormData();
 
     formData.append("suser.LOGIN", User.LOGIN);
     formData.append("suser.PWD", User.PWD);
     formData.append("suser.ROLE", User.ROLE);
-    formData.append("suser.IDPROJET", User.IDSOCIETE);
+
+    //if (!id) {
+    //    formData.append("suser.IDPROJET", User.IDPROJET);
+    //} else {
+    //    formData.append("suser.IDPROJET", id);
+    //}
 
     formData.append("iProjet", $("#proj").val());
 
     $.ajax({
         type: "POST",
-        url: Origin + '/Traitement/GenerationSIIGLOADOTHER',
+        url: Origin + '/Traitement/GenerationSIIGLOAD',
         data: formData,
         cache: false,
         contentType: false,
         processData: false,
         success: function (result) {
             var Datas = JSON.parse(result);
-            console.log(Datas);
 
             if (Datas.type == "error") {
                 alert(Datas.msg);
+
                 return;
             }
             if (Datas.type == "login") {
                 alert(Datas.msg);
+
                 window.location = window.location.origin;
+
                 return;
             }
 
             if (Datas.type == "success") {
-                listResult = Datas.data;
+                listResult = Datas.data
+                contentpaie = ``;
 
                 const data = [];
 
-                $.each(listResult, function (k, v) {
+                console.log(listResult);
+
+                $.each(listResult, function (_, v) {
                     data.push({
                         id: v.No,
                         soa: v.SOA,
@@ -58,9 +68,12 @@ function GetListLOADOTHER() {
                         dateDEF: formatDate(v.DATEDEF),
                         dateTEF: formatDate(v.DATETEF),
                         dateBE: formatDate(v.DATEBE),
+                        utilisateur: v.LIEN,
+                        dateGeneration: formatDate(v.DATECREATION),
                         imputation: '',
                         piecesJustificatives: '',
-                        document: ''
+                        document: '',
+                        rejeter: ''
                     });
                 });
 
@@ -92,6 +105,8 @@ function GetListLOADOTHER() {
                         { data: 'dateDEF' },
                         { data: 'dateTEF' },
                         { data: 'dateBE' },
+                        { data: 'utilisateur' },
+                        { data: 'dateGeneration' },
                         {
                             data: 'imputation',
                             render: function (_, _, row, _) {
@@ -121,14 +136,24 @@ function GetListLOADOTHER() {
                                     </div>
                                 `;
                             }
-                        } 
+                        },
+                        {
+                            data: 'rejeter',
+                            render: function (_, _, row, _) {
+                                return `
+                                    <div onclick="modalREJET('${row.id}')">
+                                        <i class="fa fa-times fa-lg text-dark"></i>
+                                    </div>
+                                `;
+                            }
+                        }
                     ],
                     createdRow: function (row, _, _) {
                         $(row).addClass('select-text');
                     },
                     columnDefs: [
                         {
-                            targets: [-3, -2, -1],
+                            targets: [-4, -3, -2, -1],
                             className: 'elerfr'
                         }
                     ],
@@ -192,7 +217,7 @@ function GetListProjet() {
 
             $(`[data-id="proj-list"]`).append(code);
 
-            GetListLOADOTHER();
+            GetListLOAD();
         },
         error: function (e) {
             alert("Problème de connexion. ");
@@ -219,7 +244,7 @@ $('#proj').on('change', () => {
     $('.traitementORDSEC').html('');
     $('.traitementORDSECOTHER').html('');
 
-    GetListLOADOTHER();
+    GetListLOAD();
 });
 
 $('[data-action="GenereR"]').click(async function () {
@@ -400,12 +425,17 @@ $('[data-action="GenereR"]').click(async function () {
     });
 });
 
-$('[data-action="GenereSIIGOTHER"]').click(function () {
+$('[data-action="GenereSIIG"]').click(function () {
     let dd = $("#dateD").val();
     let df = $("#dateF").val();
     if (!dd || !df) {
         alert("Veuillez renseigner les dates afin de générer les mandats. ");
+        return;
+    }
 
+    let pr = $("#proj").val();
+    if (!pr) {
+        alert("Veuillez sélectionner au moins un projet. ");
         return;
     }
 
@@ -423,128 +453,64 @@ $('[data-action="GenereSIIGOTHER"]').click(function () {
 
     $.ajax({
         type: "POST",
-        url: Origin + '/Traitement/GenerationSIIGOTHER',
+        url: Origin + '/Traitement/GenerationSIIG',
         data: formData,
         cache: false,
         contentType: false,
         processData: false,
         success: function (result) {
             var Datas = JSON.parse(result);
+            console.log(Datas);
 
             if (Datas.type == "error") {
                 alert(Datas.msg);
-
                 return;
             }
             if (Datas.type == "login") {
                 alert(Datas.msg);
                 window.location = window.location.origin;
-
                 return;
             }
             if (Datas.type == "success") {
-                listResult = Datas.data;
-
-                const data = [];
-
+                listResult = Datas.data
+                contentpaie = ``;
                 $.each(listResult, function (k, v) {
-                    data.push({
-                        id: v.No,
-                        soa: v.SOA,
-                        projet: v.PROJET,
-                        ref: v.REF,
-                        objet: v.OBJ,
-                        titulaire: v.TITUL,
-                        dateMandat: formatDate(v.DATE),
-                        compte: v.COMPTE,
-                        pcop: v.PCOP,
-                        montant: formatCurrency(String(v.MONT)),
-                        dateDEF: formatDate(v.DATEDEF),
-                        dateTEF: formatDate(v.DATETEF),
-                        dateBE: formatDate(v.DATEBE),
-                        imputation: '',
-                        piecesJustificatives: '',
-                        document: ''
-                    });
+                    contentpaie += `
+                    <tr compteG-id="${v.No}" class="select-text">
+                        <td style="font-weight: bold; text-align:center">
+                            <input type="checkbox" name = "checkprod" compteg-ischecked  onchange = "checkdel()"/>
+                        </td>
+                        <td style="font-weight: bold; text-align:center">${v.SOA}</td>
+                        <td style="font-weight: bold; text-align:center">${v.PROJET}</td>
+                        <td style="font-weight: bold; text-align:center">${v.REF}</td>
+                        <td style="font-weight: bold; text-align:center">${v.OBJ}</td>
+                        <td style="font-weight: bold; text-align:center">${v.TITUL}</td>
+                        <td style="font-weight: bold; text-align:center">${formatDate(v.DATE)}</td>
+                        <td style="font-weight: bold; text-align:center">${v.COMPTE}</td>
+                        <td style="font-weight: bold; text-align:center">${v.PCOP}</td>
+                        <td style="font-weight: bold; text-align:center">${formatCurrency(String(v.MONT).replace(",", "."))}</td>
+                        <td style="font-weight: bold; text-align:center">${formatDate(v.DATEDEF)}</td>
+                        <td style="font-weight: bold; text-align:center">${formatDate(v.DATETEF)}</td>
+                        <td style="font-weight: bold; text-align:center">${formatDate(v.DATEBE)}</td>
+                        <td style="font-weight: bold; text-align:center">${v.LIEN}</td>
+                        <td style="font-weight: bold; text-align:center">${formatDate(v.DATECREATION)}</td>
+                        <td class="elerfr" style="font-weight: bold; text-align:center">
+                            <div onclick="modalD('${v.No}')"><i class="fa fa-tags fa-lg text-danger"></i></div>
+                        </td>
+                        <td class="elerfr" style="font-weight: bold; text-align:center">
+                            <div onclick="modalF('${v.No}')"><i class="fa fa-tags fa-lg text-success"></i></div>
+                        </td>
+                        <td class="elerfr" style="font-weight: bold; text-align:center">
+                            <div onclick="modalLIAS('${v.No}')"><i class="fa fa-tags fa-lg text-info"></i></div>
+                        </td>
+                        <td class="elerfr" style="font-weight: bold; text-align:center">
+                            <div onclick="modalREJET('${v.No}')"><i class="fa fa-times fa-lg text-dark"></i></div>
+                        </td>
+                    </tr>`
                 });
 
-                if (table !== undefined) {
-                    table.destroy();
-                }
-
-                table = $('#TBD_PROJET').DataTable({
-                    data,
-                    columns: [
-                        {
-                            data: 'id',
-                            render: function (data, _, _, _) {
-                                return `
-                                    <input type="checkbox" name="checkprod" compteg-ischecked class="chk" onchange="checkdel('${data}')" />
-                                `;
-                            },
-                            orderable: false
-                        },
-                        { data: 'soa' },
-                        { data: 'projet' },
-                        { data: 'ref' },
-                        { data: 'objet' },
-                        { data: 'titulaire' },
-                        { data: 'dateMandat' },
-                        { data: 'compte' },
-                        { data: 'pcop' },
-                        { data: 'montant' },
-                        { data: 'dateDEF' },
-                        { data: 'dateTEF' },
-                        { data: 'dateBE' },
-                        {
-                            data: 'imputation',
-                            render: function (_, _, row, _) {
-                                return `
-                                    <div onclick="modalD('${row.id}')">
-                                        <i class="fa fa-tags fa-lg text-danger"></i>
-                                    </div>
-                                `;
-                            }
-                        },
-                        {
-                            data: 'piecesJustificatives',
-                            render: function (_, _, row, _) {
-                                return `
-                                    <div onclick="modalF('${row.id}')">
-                                        <i class="fa fa-tags fa-lg text-danger"></i>
-                                    </div>
-                                `;
-                            }
-                        },
-                        {
-                            data: 'document',
-                            render: function (_, _, row, _) {
-                                return `
-                                    <div onclick="modalLIAS('${row.id}')">
-                                        <i class="fa fa-tags fa-lg text-danger"></i>
-                                    </div>
-                                `;
-                            }
-                        }
-                    ],
-                    createdRow: function (row, _, _) {
-                        $(row).addClass('select-text');
-                    },
-                    columnDefs: [
-                        {
-                            targets: [-3, -2, -1],
-                            className: 'elerfr'
-                        }
-                    ],
-                    colReorder: {
-                        enable: true,
-                        fixedColumnsLeft: 1
-                    },
-                    deferRender: true,
-                    initComplete: function () {
-                        $(`thead td[data-column-index="${0}"]`).removeClass('sorting_asc').removeClass('sorting_desc');
-                    }
-                });
+                $('.traitementORDSEC').empty();
+                $('.traitementORDSEC').html(contentpaie);
             }
         },
         error: function () {
--- apptab/Assets/js/utils.js
+++ apptab/Assets/js/utils.js
@@ -1,7 +1,11 @@
 function formatCurrency(amount) {
-    return new Intl.NumberFormat("fr-FR").format(amount);
+    return new Intl.NumberFormat("fr-FR").format(amount).replace(',', '.');
 }
 
 function formatDate(date) {
+    if (!dayjs(date).isValid()) {
+        return '';
+    }
+
     return dayjs(date).format('DD/MM/YYYY');
 }
