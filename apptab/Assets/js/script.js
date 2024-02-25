0144522226877ad3200122e786faa95036dc1126
Rinah6 <fenosoaraharinosy@gmail.com>
2024-02-25 17:40:33 +0300

    RFR

--- apptab/Assets/js/script.js
+++ apptab/Assets/js/script.js
@@ -2,3 +2,19 @@
     sessionStorage.setItem("user", null);
     window.location = window.location.origin;
 }
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return;
+};
