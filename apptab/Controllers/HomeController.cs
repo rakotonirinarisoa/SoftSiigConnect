3b27a4ad1c337283f50f9be16ecd2c16923466a8
rakotonirinarisoa <rakotonirinarisoanj@gmail.com>
2024-03-13 11:00:32 +0300

    RAF

--- apptab/Controllers/HomeController.cs
+++ apptab/Controllers/HomeController.cs
@@ -34,22 +34,28 @@ namespace apptab.Controllers
 
         public ActionResult TdbAccueil()
         {
+            
             return View();
         }
         public ActionResult PaiementValidation()
         {
+            
+            ViewBag.Controller = "Récupération des paiements";
             return View();
         }
         public ActionResult TeacherValidation()
         {
+            ViewBag.Controller = "Envoi pour validation";
             return View();
         }
         public ActionResult FValidation()
         {
+            ViewBag.Controller = "Génération fichier banque";
             return View();
         }
         public ActionResult BonPourPaiement()
         {
+            ViewBag.Controller = "Validation comptable payeur";
             return View();
         }
         [HttpPost]
