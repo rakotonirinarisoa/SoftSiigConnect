ab28c2516caab0614b55e49a3617251744e2743b
Rinah6 <fenosoaraharinosy@gmail.com>
2024-03-16 11:28:17 +0300

    RFR

--- apptab/Assets/js/TraitmandatOA.js
+++ apptab/Assets/js/TraitmandatOA.js
@@ -220,16 +220,80 @@ function GetListLOADOTHER() {
                         }
                     ],
                     colReorder: {
-                        enable: true,
+                        enable: false,
                         fixedColumnsLeft: 1
                     },
                     deferRender: true,
                     dom: 'Bfrtip',
                     buttons: ['colvis'],
+
+                    /*fixedColumns: true,*/
                     initComplete: function () {
                         $(`thead td[data-column-index="${0}"]`).removeClass('sorting_asc').removeClass('sorting_desc');
+
+                        count = 0;
+                        this.api().columns().every(function () {
+                            var title = this.header();
+                            //replace spaces with dashes
+                            title = $(title).html().replace(/[\W]/g, '-');
+                            var column = this;
+                            var select = $('<select id="' + title + '" class="select2" ></select>')
+                                .appendTo($(column.footer()).empty())
+                                .on('change', function () {
+                                    //Get the "text" property from each selected data 
+                                    //regex escape the value and store in array
+                                    var data = $.map($(this).select2('data'), function (value, key) {
+                                        return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                    });
+
+                                    //if no data selected use ""
+                                    if (data.length === 0) {
+                                        data = [""];
+                                    }
+
+                                    //join array into string with regex or (|)
+                                    var val = data.join('|');
+
+                                    //search for the option(s) selected
+                                    column
+                                        .search(val ? val : '', true, false)
+                                        .draw();
+                                });
+
+                            column.data().unique().sort().each(function (d, j) {
+                                //if (j!=0) {
+                                //    select.append('<option value="' + d + '">' + d + '</option>');
+                                //}
+                                select.append('<option value="' + d + '">' + d + '</option>');
+                            });
+
+                            //use column title as selector and placeholder
+                            $('#' + title).select2({
+                                multiple: true,
+                                closeOnSelect: false,
+                                //placeholder: /*"Select a " + */title,
+                                //enable: true
+                                
+                            });
+
+                            //initially clear select otherwise first option is selected
+                            $('.select2').val(null).trigger('change');
+                        });
                     }
                 });
+
+                $('#TBD_PROJET_OTHER tfoot th').each(function (i) {
+                    if (i == 0 || i >= 13) {
+                        $(this).addClass("NOTVISIBLE");
+                    }
+                });
+
+                //$(table.table().container()).on('keyup', 'tfoot input', function () {
+                //    table
+                //        .column($(this).data('index'))
+                //        .search(this.value)
+                //        .draw();
+                //});
             }
         },
         error: function () {
@@ -399,6 +463,56 @@ $('[data-action="GenereSIIGOTHER"]').click(function () {
                     buttons: ['colvis'],
                     initComplete: function () {
                         $(`thead td[data-column-index="${0}"]`).removeClass('sorting_asc').removeClass('sorting_desc');
+
+                        count = 0;
+                        this.api().columns().every(function () {
+                            var title = this.header();
+                            //replace spaces with dashes
+                            title = $(title).html().replace(/[\W]/g, '-');
+                            var column = this;
+                            var select = $('<select id="' + title + '" class="select2" ></select>')
+                                .appendTo($(column.footer()).empty())
+                                .on('change', function () {
+                                    //Get the "text" property from each selected data 
+                                    //regex escape the value and store in array
+                                    var data = $.map($(this).select2('data'), function (value, key) {
+                                        return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                    });
+
+                                    //if no data selected use ""
+                                    if (data.length === 0) {
+                                        data = [""];
+                                    }
+
+                                    //join array into string with regex or (|)
+                                    var val = data.join('|');
+
+                                    //search for the option(s) selected
+                                    column
+                                        .search(val ? val : '', true, false)
+                                        .draw();
+                                });
+
+                            column.data().unique().sort().each(function (d, j) {
+                                select.append('<option value="' + d + '">' + d + '</option>');
+                            });
+
+                            //use column title as selector and placeholder
+                            $('#' + title).select2({
+                                multiple: true,
+                                closeOnSelect: false
+
+                            });
+
+                            //initially clear select otherwise first option is selected
+                            $('.select2').val(null).trigger('change');
+                        });
+                    }
+                });
+
+                $('#TBD_PROJET_OTHER tfoot th').each(function (i) {
+                    if (i == 0 || i >= 13) {
+                        $(this).addClass("NOTVISIBLE");
                     }
                 });
             }
