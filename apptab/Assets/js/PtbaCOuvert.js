772155662e9933b2e3b4ae2a3e34fad4e1951828
Rinah6 <fenosoaraharinosy@gmail.com>
2024-05-25 21:31:12 +0300

    RFR

---
+++ apptab/Assets/js/PtbaCOuvert.js
@@ -0,0 +1,358 @@
+﻿var table = undefined;
+
+$(document).ready(() => {
+    User = JSON.parse(sessionStorage.getItem("user"));
+    if (User == null || User === "undefined") window.location = User.origin;
+    Origin = User.origin;
+
+    $(`[data-id="username"]`).text(User.LOGIN);
+
+    GetListProjet();
+
+    $(`[data-widget="pushmenu"]`).on('click', () => {
+        $(`[data-action="SaveV"]`).toggleClass('custom-fixed-btn');
+    });
+});
+
+$('#proj').on('change', () => {
+    emptyTable();
+});
+
+function checkdel(id) {
+    $('.Checkall').prop("checked", false);
+}
+
+function GetListProjet() {
+    let formData = new FormData();
+
+    formData.append("suser.LOGIN", User.LOGIN);
+    formData.append("suser.PWD", User.PWD);
+    formData.append("suser.ROLE", User.ROLE);
+    formData.append("suser.IDPROJET", User.IDPROJET);
+
+    $.ajax({
+        type: "POST",
+        url: Origin + '/BordTraitement/GetAllPROJET',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        beforeSend: function () {
+            loader.removeClass('display-none');
+        },
+        complete: function () {
+            loader.addClass('display-none');
+        },
+        success: function (result) {
+            var Datas = JSON.parse(result);
+
+            if (Datas.type == "error") {
+                alert(Datas.msg);
+                return;
+            }
+            if (Datas.type == "login") {
+                alert(Datas.msg);
+                window.location = window.location.origin;
+                return;
+            }
+
+            $(`[data-id="proj-list"]`).text("");
+            var code = ``;
+            $.each(Datas.data.List, function (k, v) {
+                code += `
+                    <option value="${v.ID}">${v.PROJET}</option>
+                `;
+            });
+
+            $(`[data-id="proj-list"]`).append(code);
+
+            $("#proj").val([...Datas.data.PROJET]).trigger('change');
+        },
+        error: function (e) {
+            alert("Problème de connexion. ");
+        }
+    })
+}
+
+$('.Checkall').change(function () {
+
+    if ($('.Checkall').prop("checked") == true) {
+
+        $('[compteg-ischecked]').prop("checked", true);
+    } else {
+        $('[compteg-ischecked]').prop("checked", false);
+    }
+
+});
+
+function emptyTable() {
+    const data = [];
+
+    if (table !== undefined) {
+        table.destroy();
+    }
+
+    table = $('#TBD_PROJET_ORDSEC').DataTable({
+        data,
+        colReorder: {
+            enable: false,
+            fixedColumnsLeft: 1
+        },
+        deferRender: true,
+        dom: 'Bfrtip',
+        buttons: ['colvis'],
+        pageLength: 25,
+    });
+}
+
+$('[data-action="GenereLISTE"]').click(function () {
+    let dd = $("#dateD").val();
+    let df = $("#dateF").val();
+    if (!dd || !df) {
+        alert("Veuillez renseigner les dates afin de générer la liste. ");
+        return;
+    }
+
+    let pr = $("#proj").val();
+    if (!pr) {
+        alert("Veuillez sélectionner au moins un projet. ");
+        return;
+    }
+
+    let formData = new FormData();
+
+    formData.append("suser.LOGIN", User.LOGIN);
+    formData.append("suser.PWD", User.PWD);
+    formData.append("suser.ROLE", User.ROLE);
+    formData.append("suser.IDPROJET", User.IDSOCIETE);
+
+    formData.append("DateDebut", $('#dateD').val());
+    formData.append("DateFin", $('#dateF').val());
+
+    formData.append("listProjet", $("#proj").val());
+
+    $.ajax({
+        type: "POST",
+        url: Origin + '/BordTraitement/GenerePTBA',
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        beforeSend: function () {
+            loader.removeClass('display-none');
+        },
+        complete: function () {
+            loader.addClass('display-none');
+        },
+        success: function (result) {
+            var Datas = JSON.parse(result);
+
+            if (Datas.type == "error") {
+                alert(Datas.msg);
+                emptyTable();
+                return;
+            }
+            if (Datas.type == "login") {
+                alert(Datas.msg);
+                window.location = window.location.origin;
+                return;
+            }
+            if (Datas.type == "PEtat") {
+                alert(Datas.msg);
+                emptyTable();
+                return;
+            }
+            if (Datas.type == "Prese") {
+                alert(Datas.msg);
+                emptyTable();
+                return;
+            }
+            if (Datas.type == "success") {
+                listResult = Datas.data
+
+                const data = [];
+
+                $.each(listResult, function (_, v) {
+                    data.push({
+                        id: v.No,
+                        SOA: v.SOA,
+                        PROJET: v.PROJET,
+                        PCOP: v.REF,
+                        CREDITOUVERT: formatCurrency(String(v.BENEF).replace(",", ".")),
+                        MTNPTBA: formatCurrency(String(v.MONTENGAGEMENT).replace(",", ".")),
+                        MTNANGAGE: formatCurrency(String(v.MONTPAIE).replace(",", ".")),
+                        MTNPAYE: formatCurrency(String(v.TYPE).replace(",", ".")),
+                        SOLDECREDITOUVERT: formatCurrency(String(v.NPIECE).replace(",", ".")),
+                        PCREDITOUVERT: formatCurrency(String(v.AGENTREJETE).replace(",", ".")),
+                        SOLDEPTBA: formatCurrency(String(v.MOTIF).replace(",", ".")),
+                        PPTBA: formatCurrency(String(v.COMMENTAIRE).replace(",", "."))
+                    });
+                });
+
+                if (table !== undefined) {
+                    table.destroy();
+                }
+
+                table = $('#TBD_PROJET_ORDSEC').DataTable({
+                    data,
+                    columns: [
+                        {
+                            data: 'id',
+                            render: function (data, _, _, _) {
+                                return `
+                                    <input type="checkbox" name="checkprod" compteg-ischecked class="chk" onchange="checkdel('${data}')" />
+                                `;
+                            },
+                            orderable: false
+                        },
+                        { data: 'SOA' },
+                        { data: 'PROJET' },
+                        { data: 'PCOP' },
+                        { data: 'CREDITOUVERT' },
+                        { data: 'MTNPTBA' },
+                        { data: 'MTNANGAGE' },
+                        { data: 'MTNPAYE' },
+                        { data: 'SOLDECREDITOUVERT' },
+                        { data: 'PCREDITOUVERT' },
+                        { data: 'SOLDEPTBA' },
+                        { data: 'PPTBA' },
+                    ],
+                    createdRow: function (row, data, _) {
+                        $(row).attr('compteG-id', data.id);
+                        $(row).addClass('select-text');
+
+                        //if (data.isLATE) {
+                        //    $(row).attr('style', "background-color: #FF7F7F !important;");
+                        //}
+                    },
+                    columnDefs: [
+                        {
+                            targets: [-4, -3, -2, -1]
+                        }
+                    ],
+                    colReorder: {
+                        enable: false,
+                        fixedColumnsLeft: 1
+                    },
+                    deferRender: true,
+                    dom: 'Bfrtip',
+                    buttons: ['colvis'],
+                    pageLength: 25,
+                    caption: 'SOFT - SIIG CONNECT ' + new Date().toLocaleDateString(),
+                    buttons: ['colvis',
+                        {
+                            extend: 'pdfHtml5',
+                            title: 'SUIVI BUDGETAIRE ET CREDIT OUVERT',
+                            messageTop: 'Suivi budgétaire et crédit ouvert',
+                            text: '<i class="fa fa-file-pdf"> Exporter en PDF</i>',
+                            orientation: 'landscape',
+                            pageSize: 'A4',
+                            charset: "utf-8",
+                            bom: true,
+                            className: 'custombutton-collection-pdf',
+                            exportOptions: {
+                                columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                            },
+                            customize: function (doc) {
+                                doc.defaultStyle.alignment = 'left';
+                                //doc.defaultStyle.margin = [12, 12, 12, 12];
+                            },
+                            download: 'open'
+                        },
+                        {
+                            extend: 'excelHtml5',
+                            title: 'SUIVI BUDGETAIRE ET CREDIT OUVERT',
+                            messageTop: 'Suivi budgétaire et crédit ouvert',
+                            text: '<i class="fa fa-file-excel"> Exporter en Excel</i>',
+                            orientation: 'landscape',
+                            pageSize: 'A4',
+                            charset: "utf-8",
+                            bom: true,
+                            className: 'custombutton-collection-excel',
+                            exportOptions: {
+                                columns: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+                                format: {
+                                    body: function (data, row, column, node) {
+                                        if (typeof data === 'undefined') {
+                                            return;
+                                        }
+                                        if (data == null) {
+                                            return data;
+                                        }
+                                        if (column === 4 || column === 5 || column === 6 || column === 7 || column === 8 || column === 10) {
+                                            var arr = data.split(',');
+                                            arr[0] = arr[0].toString().replace(/[\.]/g, "");
+                                            if (arr[0] > '' || arr[1] > '') {
+                                                data = arr[0] + '.' + arr[1];
+                                            } else {
+                                                return '';
+                                            }
+                                            return data.toString().replace(/[^\d.-]/g, "");
+                                        }
+                                        return data;
+                                    }
+                                }
+                            },
+                        }
+                    ],
+                    initComplete: function () {
+                        $(`thead td[data-column-index="${0}"]`).removeClass('sorting_asc').removeClass('sorting_desc');
+
+                        count = 0;
+                        this.api().columns().every(function () {
+                            var title = this.header();
+                            //replace spaces with dashes
+                            title = $(title).html().replace(/[\W]/g, '-');
+                            var column = this;
+                            var select = $('<select id="' + title + '" class="select2" ></select>')
+                                .appendTo($(column.footer()).empty())
+                                .on('change', function () {
+                                    //Get the "text" property from each selected data 
+                                    //regex escape the value and store in array
+                                    var data = $.map($(this).select2('data'), function (value, key) {
+                                        return value.text ? '^' + $.fn.dataTable.util.escapeRegex(value.text) + '$' : null;
+                                    });
+
+                                    //if no data selected use ""
+                                    if (data.length === 0) {
+                                        data = [""];
+                                    }
+
+                                    //join array into string with regex or (|)
+                                    var val = data.join('|');
+
+                                    //search for the option(s) selected
+                                    column
+                                        .search(val ? val : '', true, false)
+                                        .draw();
+                                });
+
+                            column.data().unique().sort().each(function (d, j) {
+                                select.append('<option value="' + d + '">' + d + '</option>');
+                            });
+
+                            //use column title as selector and placeholder
+                            $('#' + title).select2({
+                                multiple: true,
+                                closeOnSelect: false
+
+                            });
+
+                            //initially clear select otherwise first option is selected
+                            $('.select2').val(null).trigger('change');
+                        });
+                    }
+                });
+
+                $('#TBD_PROJET_ORDSEC tfoot th').each(function (i) {
+                    if (i == 0) {
+                        $(this).addClass("NOTVISIBLE");
+                    }
+                });
+            }
+        },
+        error: function () {
+            alert("Problème de connexion. ");
+        }
+    });
+});
--- apptab/Controllers/BordTraitementController.cs
+++ apptab/Controllers/BordTraitementController.cs
@@ -1924,5 +1924,97 @@ namespace apptab.Controllers
                 return Json(JsonConvert.SerializeObject(new { type = "error", msg = e.Message }, settings));
             }
         }
+
+        //Suivi budgétaire et crédit ouvert//
+        public ActionResult BudgetCreditOuvert()
+        {
+            ViewBag.Controller = "Suivi budgétaire et crédit ouvert";
+
+            return View();
+        }
+
+        //GENERER Suivi budgétaire et crédit ouvert//
+        [HttpPost]
+        public JsonResult GenerePTBA(SI_USERS suser, string listProjet, DateTime DateDebut, DateTime DateFin)
+        {
+            var exist = db.SI_USERS.FirstOrDefault(a => a.LOGIN == suser.LOGIN && a.PWD == suser.PWD && a.DELETIONDATE == null/* && a.IDSOCIETE == suser.IDSOCIETE*/);
+            if (exist == null) return Json(JsonConvert.SerializeObject(new { type = "login", msg = "Problème de connexion. " }, settings));
+
+            try
+            {
+                List<TxLISTETRAIT> list = new List<TxLISTETRAIT>();
+                string[] separators = { "," };
+                var pro = listProjet;
+                if (pro != null)
+                {
+                    string listUser = pro.ToString();
+                    string[] lst = listUser.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var idP in lst)
+                    {
+                        int crpt = int.Parse(idP);
+
+                        //int retarDate = 0;
+                        //if (db.SI_DELAISTRAITEMENT.Any(a => a.IDPROJET == crpt && a.DELETIONDATE == null))
+                        //    retarDate = db.SI_DELAISTRAITEMENT.FirstOrDefault(a => a.IDPROJET == crpt && a.DELETIONDATE == null).DELTV.Value;
+
+                        //Check si le projet est mappé à une base de données TOM²PRO//
+                        if (db.SI_MAPPAGES.FirstOrDefault(a => a.IDPROJET == crpt && a.DELETIONDATE == null) == null)
+                            return Json(JsonConvert.SerializeObject(new { type = "error", msg = "Le projet n'est pas mappé à une base de données TOM²PRO. " }, settings));
+
+                        SOFTCONNECTOM.connex = new Data.Extension().GetCon(crpt);
+                        SOFTCONNECTOM tom = new SOFTCONNECTOM();
+
+                        //Check si la correspondance des états est OK//
+                        var numCaEtapAPP = db.SI_PARAMETAT.FirstOrDefault(a => a.IDPROJET == crpt && a.DELETIONDATE == null);
+                        if (numCaEtapAPP == null) return Json(JsonConvert.SerializeObject(new { type = "PEtat", msg = "Veuillez paramétrer la correspondance des états. " }, settings));
+                        //TEST si les états dans les paramètres dans cohérents avec ceux de TOM²PRO//
+                        if (tom.CPTADMIN_CHAINETRAITEMENT.FirstOrDefault(a => a.NUM == numCaEtapAPP.DEF) == null)
+                            return Json(JsonConvert.SerializeObject(new { type = "Prese", msg = "L'état DEF n'est pas paramétré sur TOM²PRO (Liquidation). " }, settings));
+                        if (tom.CPTADMIN_CHAINETRAITEMENT.FirstOrDefault(a => a.NUM == numCaEtapAPP.TEF) == null)
+                            return Json(JsonConvert.SerializeObject(new { type = "Prese", msg = "L'état TEF n'est pas paramétré sur TOM²PRO (Liquidation). " }, settings));
+                        if (tom.CPTADMIN_CHAINETRAITEMENT.FirstOrDefault(a => a.NUM == numCaEtapAPP.BE) == null)
+                            return Json(JsonConvert.SerializeObject(new { type = "Prese", msg = "L'état BE n'est pas paramétré sur TOM²PRO (Liquidation). " }, settings));
+                    }
+
+                    foreach (var idP in lst)
+                    {
+                        int crpt = int.Parse(idP);
+
+                        var soa = (from soas in db.SI_SOAS
+                                   join prj in db.SI_PROSOA on soas.ID equals prj.IDSOA
+                                   where prj.IDPROJET == crpt && prj.DELETIONDATE == null && soas.DELETIONDATE == null
+                                   select new
+                                   {
+                                       soas.SOA
+                                   }).FirstOrDefault();
+
+                        list.Add(new TxLISTETRAIT
+                        {
+                            //No = "",//ID
+                            //SOA = soa != null ? soa.SOA : "",//SOA
+                            //PROJET = db.SI_PROJETS.Where(a => a.ID == crpt && a.DELETIONDATE == null).FirstOrDefault().PROJET,//PROJET
+
+                            //REF = "",//PCOP
+
+                            //BENEF = "",//Crédit ouvert
+                            //MONTENGAGEMENT = "",//Montant PTBA
+                            //MONTPAIE = "",//Montant engagé
+                            //TYPE = "",//Montant payé
+                            //NPIECE = "",//Solde sur crédit ouvert
+                            //AGENTREJETE = "",//% sur crédit ouvert
+                            //MOTIF = "",//Solde sur PTBA
+                            //COMMENTAIRE = "",//% sur PTBA
+                        });
+                    }
+                }
+
+                return Json(JsonConvert.SerializeObject(new { type = "success", msg = "Connexion avec succès. ", data = list.OrderByDescending(a => a.DATENGAGEMENT).ToList() }, settings));
+            }
+            catch (Exception e)
+            {
+                return Json(JsonConvert.SerializeObject(new { type = "error", msg = e.Message }, settings));
+            }
+        }
     }
 }
