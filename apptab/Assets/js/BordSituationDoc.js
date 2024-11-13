fbcd4ff13bfd4ac906ab2c3efe020df0d2b13334
rakotonirinarisoa <rakotonirinarisoanj@gmail.com>
2024-11-13 19:05:37 +0300

    FIDERANA NIFSTATCIN NoN OK

--- apptab/Assets/js/BordSituationDoc.js
+++ apptab/Assets/js/BordSituationDoc.js
@@ -31,8 +31,12 @@ $('#site').on('change', () => {
 });
 function GetSITE() {
     let formData = new FormData();
-
-    formData.append("iProjet", $("#proj").val());
+    let pr = $("#proj").val();
+    if (!pr) {
+        alert("Veuillez sélectionner au moins un projet. ");
+        return;
+    }
+    formData.append("iProjet", pr);
 
     formData.append("suser.LOGIN", User.LOGIN);
     formData.append("suser.PWD", User.PWD);
@@ -134,8 +138,12 @@ $("#site").on('change', handleSelectAll);
 
 function GetNifSTATCIN(id) {
     let formData = new FormData();
-
-    formData.append("PROJECTID", $("#proj").val());
+    let pr = $("#proj").val();
+    if (!pr) {
+        alert("Veuillez sélectionner au moins un projet. ");
+        return;
+    }
+    formData.append("PROJECTID", pr);
 
     formData.append("suser.LOGIN", User.LOGIN);
     formData.append("suser.PWD", User.PWD);
@@ -180,13 +188,13 @@ function GetNifSTATCIN(id) {
             var code3 = ``;
             $.each(Datas.data, function (k, v) {
                 code1 += `
-                    <option value="${v.ID}">${v.NIF}</option>
+                    <option value="${v.Id}">${v.NIF}</option>
                 `;
                 code2 += `
-                    <option value="${v.ID}">${v.STAT}</option>
+                    <option value="${v.Id}">${v.STAT}</option>
                 `;
                 code3 += `
-                    <option value="${v.ID}">${v.CIN}</option>
+                    <option value="${v.Id}">${v.CIN}</option>
                 `;
             });
             $(`[data-id="Nif-list"]`).append(code1);
