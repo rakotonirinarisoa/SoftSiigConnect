119752129515c0b450d60b0817a77c7be7ed30c6
Rinah6 <fenosoaraharinosy@gmail.com>
2025-02-10 09:16:34 +0300

    RFR

--- apptab/Controllers/UserController.cs
+++ apptab/Controllers/UserController.cs
@@ -59,7 +59,6 @@ namespace apptab.Controllers
             try
             {
                 var test = db.SI_USERS.Where(x => x.LOGIN == exist.LOGIN && x.ROLE == exist.ROLE && x.DELETIONDATE == null).FirstOrDefault();
-                //var test = db.SI_USERS.Where(x => x.ROLE == suser.ROLE && x.IDPROJET == suser.IDPROJET).FirstOrDefault();
                 if (test.ROLE == (int)Role.SAdministrateur)
                 {
                     var adminUser = db.SI_USERS.Where(x => x.ROLE != Role.SAdministrateur && x.DELETIONDATE == null).ToList();
@@ -285,7 +284,6 @@ namespace apptab.Controllers
             try
             {
                 var userExist = db.SI_USERS.FirstOrDefault(a => a.LOGIN == user.LOGIN && a.DELETIONDATE == null/* && a.IDPROJET == exist.IDPROJET*/);
-                //var test = db.SI_USERS.Where(x => x.ROLE == exist.ROLE && x.IDPROJET == exist.IDPROJET && x.DELETIONDATE == null).FirstOrDefault();
                 if (userExist == null)
                 {
                     if (db.SI_USERS.Any(a => a.LOGIN == user.LOGIN && a.DELETIONDATE == null))
@@ -393,7 +391,6 @@ namespace apptab.Controllers
             {
                 int userId = int.Parse(UserId);
                 var userExist = db.SI_USERS.FirstOrDefault(a => a.ID == userId && a.DELETIONDATE == null);
-                //var test = db.SI_USERS.Where(x => x.ROLE == exist.ROLE && x.IDPROJET == exist.IDPROJET && x.DELETIONDATE == null).FirstOrDefault();
                 if (userExist != null)
                 {
                     if (userExist.PWD != oldPassword) return Json(JsonConvert.SerializeObject(new { type = "error", msg = "Ancien mot de passe non valide. ", data = user }, settings));
